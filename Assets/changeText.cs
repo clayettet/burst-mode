73b3fa0e9d8dc896f0825dc1a63f2c76fab1ea17
Theo <theo-clayette@orange.fr>
2018-11-07 17:56:55 +0100

    Added random letter display and a try of camera shake (not working)

--- Assets/changeText.cs
+++ Assets/changeText.cs
@@ -8,6 +8,7 @@ public class changeText : MonoBehaviour
     public Text DisplayedText;
 
     private string LetterToWrite;
+    string[] Alphabet = new string[26] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
 
     void Start()
     {
@@ -29,12 +30,14 @@ public class changeText : MonoBehaviour
         Event e = Event.current;
         if (e.isKey)
         {
-            if (e.keyCode.ToString() == LetterToWrite)
+            if (e.keyCode.ToString() == LetterToWrite) //if letter typed is correct
             {
                 DisplayedText.text = "Good job";
+                LetterToWrite = Alphabet[Random.Range(0, Alphabet.Length)];
+                DisplayedText.text = LetterToWrite;
             } else
             {
-                //camera shake
+                GetComponent<CameraShake>().ShakeCamera(20f, 1f);
                 //sound
             }
         }
